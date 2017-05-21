bb45ab754e518b175ae1ec62815cfe3c68868ac0
corella03 <lcorellac@ucatolica.ac.cr>
2017-05-21 11:09:42 -0600

    Inicio

---
+++ src/Logic/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Paquete
+package Logic;
+/**
+ **
+ ** @author Luis Alonso Corella Chaves
+ ** @date 2017-05-21 Domingo
+ **/
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }  
+}
